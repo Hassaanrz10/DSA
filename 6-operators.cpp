5e85257a5afbbd6b97a712a4dc9531800b6da09c
Hassaan Raza <hassaanr721@gmail.com>
2025-07-19 13:06:57 +0530

    Add files via upload

---
+++ 6-operators.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//    cout<<13.2*4;
+// pre and post increament 
+   // int a=10;
+   // cout<<a++<<endl;
+   // cout<<a<<endl;
+   // int c=11;
+   // int d=c--;
+   // cout<<d<<" "<<c;
+
+   //comparison operators
+   // 1 or 0
+
+   // int a,b;
+   // cout<<"Enter the value of a: ";
+   // cin>>a;
+   // cout<<"Enter the value of b: ";
+   // cin>>b;
+
+   // ==
+
+   // if(a==b)
+   // cout<<"yes";
+   // else
+   // cout<<"no";
+
+   // >,<,>=,<=,!=
+   
+   // if(a>b)
+   // cout<<"yes";
+   // else
+   // cout<<"no";
+   
+   
+   
+
+   //Logical operators
+
+   // int a,b,c;
+   // cin>>a>>b>>c;
+
+   // if(a>b&&a>c)
+   // cout<<"yes";
+   // else
+   // cout<<"no";
+
+   //Or
+
+
+   // char name='b';
+
+   // if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+   // cout<<"vowel";
+   // else
+   // cout<<"consonant";
+
+   //!
+   // cout<<!23<<endl;
+   //prints 0
+   // cout<<!0<<endl;
+   //prints 1
+
+
+    //Bitwise operator
+
+    // & operator
+   //  int ans =2&3;
+   //  cout<<ans<<endl;
+
+   // | operator
+   //  int ans =2|3;
+   //  cout<<ans<<endl;
+
+   // ^ operator
+   //  int ans =2^3;
+   //  cout<<ans<<endl;
+
+   // >> operator
+   //  int ans =2>>3;
+   //  cout<<ans<<endl;
+
+   // << operator
+   //  int ans =2<<3;
+   //  cout<<ans<<endl;
+
+   // ~ operator
+   //  int ans =2~3;
+   //  cout<<ans<<endl;
+
+
+
+   // Assingnment  operator
+
+   int a=10;
+   a+=15;  // a=a+15; same for -=,*=,etc
+   cout<<a<<endl;
+
+   if (8>5>3<2*4)
+   cout<<"Yes";
+   else
+   cout<<"No";
+
+
+
+
+
+return 0;
+}
